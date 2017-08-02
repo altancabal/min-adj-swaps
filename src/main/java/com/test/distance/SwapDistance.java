82b56ec3b8f2fc07992eec04ba6575f44d9964b0
AltanCabal <altancabal@gmail.com>
2017-08-02 08:51:04 -0600

    Move similarity minimum check inside the loop

--- src/main/java/com/test/distance/SwapDistance.java
+++ src/main/java/com/test/distance/SwapDistance.java
@@ -3,19 +3,24 @@ package com.test.distance;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * App to get the similarity value between 2 surnames
+ * @author Altan Cabal
+ * */
+
 public class SwapDistance {
 
 	/**
-	 * Get the numeric position of a char for latin alphabet (upper case). Ex.:
-	 * A -> 0, N -> 13
+	 * Get the numeric position of a char for Latin alphabet (upper case).
+	 * Ex.: A -> 0, N -> 13
 	 */
 	public static int getNumericPosition(char c) {
 		return (int) c - 65;
 	}
 
 	/**
-	 * Get the numeric representation of a char array. Ex.: [A,N,N,E] ->
-	 * [0,13,13,4]
+	 * Get the numeric representation of a char array.
+	 * Ex.: [A,N,N,E] -> [0,13,13,4]
 	 */
 	public static int[] getNumericRepresentation(char[] chars) {
 		int[] nums = new int[chars.length];
@@ -26,7 +31,7 @@ public class SwapDistance {
 	}
 
 	/**
-	 * Check if the strings are anagrams in O(n log(n))
+	 * Check if strings are anagrams
 	 * */
 	public static boolean isAnagram(String str1, String str2) {
 		char[] word1 = str1.toCharArray();
@@ -57,7 +62,7 @@ public class SwapDistance {
 
 			ArrayList<Integer>[] inverse = (ArrayList<Integer>[]) new ArrayList[alphLen];
 
-			// Inverse of str1
+			// Create dictionary where elements are keys and values are lists of indexes
 			for (int i = 0; i < strLen; i++) {
 				if (inverse[num1[i]] == null) {
 					ArrayList<Integer> tmpArray = new ArrayList<Integer>();
@@ -68,25 +73,23 @@ public class SwapDistance {
 				}
 			}
 
+			// Map second string with elements of indexes
 			int[] num2Inv = new int[strLen];
 			for (int i = 0; i < strLen; i++) {
 				num2Inv[i] = inverse[num2[i]].remove(0);
-				//System.out.println("num2Inv: " + num2Inv[i]);
 			}
 			
+			// Loop through the combinations to get the number of swaps.
+			// Each additional swap substracts 5 to the similarity value.
 			int similarity = 100;
-			for(int i = 0; i < num2Inv.length; i++){
-		        for(int j = i; j < num2Inv.length; j++){
-		            //System.out.println("i: "+num2Inv[i]+", j: "+num2Inv[j]);
+			for(int i = 0; (i < num2Inv.length) && (similarity > 0); i++){
+		        for(int j = i; (j < num2Inv.length) && (similarity > 0); j++){
 		            if(num2Inv[i] > num2Inv[j]){
 		            	similarity -= 5;
 		            }
 		        }
 		    }
 
-			if (similarity < 0) {
-				similarity = 0;
-			}
 			return similarity;
 		} else {
 			return 0;
@@ -98,25 +101,16 @@ public class SwapDistance {
 	 */
 	public static void main(String[] args) {
 		// TEST CASES
-		System.out
-				.println("(\"ABCDEF\", \"ABCDEF\") returns " + SwapDistance.getSimilarity("ABCDEF", "ABCDEF"));
-		System.out
-				.println("(\"ABCDEF\", \"BBCDEF\") returns " + SwapDistance.getSimilarity("ABCDEF", "BBCDEF"));
-		System.out
-				.println("(\"BBDCEF\", \"BBCDEF\") returns " + SwapDistance.getSimilarity("BBDCEF", "BBCDEF"));
-		System.out
-				.println("(\"HARRIS\", \"HARIRS\") returns " + SwapDistance.getSimilarity("HARRIS", "HARIRS"));
-		System.out
-				.println("(\"HARRIS\", \"HRARIS\") returns " + SwapDistance.getSimilarity("HARRIS", "HRARIS"));
-		System.out
-				.println("(\"BACDFE\", \"ABCDEF\") returns " + SwapDistance.getSimilarity("BACDFE", "ABCDEF"));
-		System.out
-				.println("(\"ABCFDE\", \"ABCDEF\") returns " + SwapDistance.getSimilarity("ABCFDE", "ABCDEF"));
+		System.out.println("(\"ABCDEF\", \"ABCDEF\") returns " + SwapDistance.getSimilarity("ABCDEF", "ABCDEF"));
+		System.out.println("(\"ABCDEF\", \"BBCDEF\") returns " + SwapDistance.getSimilarity("ABCDEF", "BBCDEF"));
+		System.out.println("(\"BBDCEF\", \"BBCDEF\") returns " + SwapDistance.getSimilarity("BBDCEF", "BBCDEF"));
+		System.out.println("(\"HARRIS\", \"HARIRS\") returns " + SwapDistance.getSimilarity("HARRIS", "HARIRS"));
+		System.out.println("(\"HARRIS\", \"HRARIS\") returns " + SwapDistance.getSimilarity("HARRIS", "HRARIS"));
+		System.out.println("(\"BACDFE\", \"ABCDEF\") returns " + SwapDistance.getSimilarity("BACDFE", "ABCDEF"));
+		System.out.println("(\"ABCFDE\", \"ABCDEF\") returns " + SwapDistance.getSimilarity("ABCFDE", "ABCDEF"));
 		System.out.println("(\"ANNE\", \"ENNA\") returns " + SwapDistance.getSimilarity("ANNE", "ENNA"));
-		System.out
-				.println("(\"FEDCBA\", \"ABCDEF\") returns " + SwapDistance.getSimilarity("FEDCBA", "ABCDEF"));
-		System.out.println(
-				"(\"GFEDCBA\", \"ABCDEFG\") returns " + SwapDistance.getSimilarity("GFEDCBA", "ABCDEFG"));
+		System.out.println("(\"FEDCBA\", \"ABCDEF\") returns " + SwapDistance.getSimilarity("FEDCBA", "ABCDEF"));
+		System.out.println("(\"GFEDCBA\", \"ABCDEFG\") returns " + SwapDistance.getSimilarity("GFEDCBA", "ABCDEFG"));
 	}
 
 }
